125263e3be86635c46b087ba7d8724946de39f32
Alin Olteanu <olteanualin1509@gmail.com>
2024-07-29 19:51:16 +0300

    Made homepage responsive

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,7 +9,7 @@ import {ReactComponent as Ilustration} from "./ilustration.svg"
 function Home() {
   return (
     <div className="home">
-      <div className="grid-2">
+      <div className="header-grid">
         <div className='header-content'>
           <div>
             <h1 className='home-header'>To-Do List</h1>
@@ -28,23 +28,25 @@ function Home() {
       </div>
       
 
-      <div className="m-top-5" id='main-content'>
+      <div className="m-top-5 container" id='main-content'>
         <h1 className='cards-header color-primary font-header'>Why use a To-Do List?</h1>
-        <div className="grid-3 gap-2 m-top-2">
+        <div className="grid-3 gap-2 m-top-2 container">
           <div className="card p-1">
             <ArticleIcon className='icon-size-4 color-primary'/>
             <h2 className='card-header'>Improved Organization</h2>
-            <p className='m-top-1'>Keep all your tasks in one place for better clarity</p>
+            <p className='m-top-1 text'>Keep all your tasks in one place for better clarity</p>
           </div>
+
           <div className="card p-1">
             <TrendingUpIcon className='icon-size-4 color-primary'/>
             <h2 className='card-header'>Increased Productivity</h2>
-            <p className='m-top-1'>Boost efficiency by breaking down tasks and tracking progress</p>
+            <p className='m-top-1 text'>Boost efficiency by breaking down tasks and tracking progress</p>
           </div>
+
           <div className="card p-1">
             <SentimentVerySatisfiedIcon className='icon-size-4 color-primary'/>
             <h2 className='card-header'>Reduced Stress</h2>
-            <p className='m-top-1'>Alleviate anxiety by having a clear plan and manageable workload</p>
+            <p className='m-top-1 text'>Alleviate anxiety by having a clear plan and manageable workload</p>
           </div>
         </div>
       </div>
