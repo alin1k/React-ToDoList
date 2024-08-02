4b845bd8e60e41e592f0f3da49fb7b3b8e22679e
Alin Olteanu <olteanualin1509@gmail.com>
2024-08-02 14:17:59 +0300

    Added 'How it Works' section on the homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,6 +5,11 @@ import ArticleIcon from '@mui/icons-material/Article';
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 import SentimentVerySatisfiedIcon from '@mui/icons-material/SentimentVerySatisfied';
 import {ReactComponent as Ilustration} from "./ilustration.svg"
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
+import BorderColorIcon from '@mui/icons-material/BorderColor';
+import PostAddIcon from '@mui/icons-material/PostAdd';
+import MoveUpIcon from '@mui/icons-material/MoveUp';
+import ChecklistIcon from '@mui/icons-material/Checklist';
 
 function Home() {
   return (
@@ -29,7 +34,7 @@ function Home() {
       
 
       <div className="m-top-5 container" id='main-content'>
-        <h1 className='cards-header color-primary font-header'>Why use a To-Do List?</h1>
+        <h1 className='heading color-primary font-heading'>Why use a To-Do List?</h1>
         <div className="grid-3 gap-2 m-top-2 container">
           <div className="card p-1">
             <ArticleIcon className='icon-size-4 color-primary'/>
@@ -50,6 +55,52 @@ function Home() {
           </div>
         </div>
       </div>
+      
+      <h1 className="heading color-primary font-heading m-top-5">How It Works</h1>
+
+      <div className="container grid-2 gap-5 row-gap-5">
+
+        <div className='text-end'>
+          <h2 className="number-heading">1</h2>
+          <h3 className='number-subheading'>Create a New List</h3>
+          <p className='text'>Start by adding a new list to organize your tasks</p>
+        </div>
+
+        <PostAddIcon className='icon-size-4 color-secondary hiw-icon'/>
+
+        <BorderColorIcon className='icon-size-4 color-secondary hiw-icon' />
+
+        <div>
+          <h2 className="number-heading">2</h2>
+          <h3 className='number-subheading'>Name Your List</h3>
+          <p className='text'>Give your list a meaningful name to keep things organized</p>
+        </div>
+
+        <div className='text-end '>
+          <h2 className="number-heading">3</h2>
+          <h3 className='number-subheading'>Add List Items</h3>
+          <p className='text'>Add new items to your list, one task at a time</p>
+        </div>
+
+        <PlaylistAddIcon className='icon-size-4 color-secondary hiw-icon'/>
+
+        <MoveUpIcon className='icon-size-4 color-secondary hiw-icon'/>
+
+        <div>
+          <h2 className="number-heading">4</h2>
+          <h3 className='number-subheading'>Organize Your Tasks</h3>
+          <p className='text'>Move items up or down in the list to prioritize your tasks</p>
+        </div>
+
+        <div className='text-end '>
+          <h2 className="number-heading">5</h2>
+          <h3 className='number-subheading'>Check Off Completed Tasks</h3>
+          <p className='text'>Mark tasks as complete by checking them off your list</p>
+        </div>
+
+        <ChecklistIcon className='icon-size-4 color-secondary hiw-icon'/>
+      </div>
+
       
 
     </div>
